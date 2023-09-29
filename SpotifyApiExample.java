3d75b9fd5bab06e5406c64e74561126295cfd285
Nick <63558865+NickieChmikie@users.noreply.github.com>
2023-09-29 12:38:51 -0400

    Update SpotifyApiExample.java

--- SpotifyApiExample.java
+++ SpotifyApiExample.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class SpotifyApiExample {
-
+    // Note that our token expires every hour, so it must be updated constantly for it to work. We use PostMan to get it done. 
     private static final String token = "BQCyeRKvG8PVjdiGsfnV9ns66jnS3eHCl5FRhAwhIxQSb_JEgr12qscfjWaylFtB23a72Twobksn0r5jWdniYl0fJMhnKtY8e0EYKIIhIK9gEmmB8WdVcdjnI4MaPWNY3KGVfqfDQBKUGPFv1My5B50QdjgUH2LMwPA_wQ8b5aiOKN6xN5EKIBHshxVpgikA-zdaIk7s61_tbdFJZadJt-KloSYpPScvRXT7BOLL4zNhQCH5SfclOsb5Rm1yCQ_0Ty52rAt4NQ";
     private static final String url = "https://api.spotify.com/v1/browse/new-releases";
 
