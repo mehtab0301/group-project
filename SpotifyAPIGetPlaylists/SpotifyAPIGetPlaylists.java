f3650bf207117711c5d83e1b3d873b0546ed8124
yuxinduan20040830 <yuxinduan0830@gmail.com>
2023-10-01 13:41:16 -0400

    Update SpotifyAPIGetPlaylists.java

--- SpotifyAPIGetPlaylists/SpotifyAPIGetPlaylists.java
+++ SpotifyAPIGetPlaylists/SpotifyAPIGetPlaylists.java
@@ -1,18 +1,24 @@
 package SpotifyAPIGetPlaylists;
 
+import java.io.*;
 import okhttp3.*;
-import java.io.IOException;
-
 public class SpotifyAPIGetPlaylists {
     public static void main(String []args) throws IOException{
+        // Note that the token will expire in one hour.
+        String token = "BQAbuG2wxNfhZMhYFegx9ZR_OclJ-6NSDLTQU9lAO_MoTcAi1imKX00fGVL3RdpLLwWYmy_34sqXdkWKb-tp77rNT7Fq0w5GAeBL41crZ7JlIE6goa82uiOmjrno7m-zC6uNJ50fGNyLYjSMd7X32ReacgR63Dakc8xYvT7AUsomk5EBvngZvioDxhHIWVL-2Yd4gYFIqQrlx2vA3Ibd1-_TC5EOhQ53lVqxTj6wiDjISkf_x3iOz6t63js-_1gm7mSdePu4GMyZaCe0FuO6YUFaJTA";
+
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
+        // RequestBody body = RequestBody.create(mediaType, "");
+
+        // Sample userID from one group member
+        String userID = "31sob5m46swjrjmpxoexdv6mzbwq";
+
         Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/users/31sob5m46swjrjmpxoexdv6mzbwq/playlists")
-                .method("GET", body)
-                .addHeader("Authorization", "Bearer BQAUe6uR9s3D9QO_SlEVBtvpxl7YFj6ZAe9EFswFavsjaHYbrYVP1j0Q2WmA79sZxTyqq55JXjh3Xd9RUogpW23d8s7xy-gWt1kx1sWmx-uXv-gGKneQzrKzipuY33YpnmlEL0VrS--2lyJ9gnZ0gEfr3BHz8zo2XEsPyJBCDn5HyhHZ8iJim2NdytM18VKRNJIcGW1ggXX3uuWu0CZIKsSUdaTs1FWL3k3XrJpeC0aKk0YRTKEm53X_TGsVMMwRwJc18TpJWXP7rNb6wHclYAeOibo")
+                .url("https://api.spotify.com/v1/users/" + userID + "/playlists")
+                // .method("GET", body)
+                .addHeader("Authorization", "Bearer " + token)
                 .build();
         Response response = client.newCall(request).execute();
         System.out.println(response.body().string());
