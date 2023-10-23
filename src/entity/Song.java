2b0d404a8cf07a614f4d2824c673b85ebd2a521f
yuxinduan20040830 <yuxinduan0830@gmail.com>
2023-10-23 15:39:52 -0400

    Set up the CA Structure

---
+++ src/entity/Song.java
@@ -0,0 +1,26 @@
+package src.entity;
+
+public class Song {
+
+    private final String name;
+    private final String artist;
+    private final int popularity;
+
+    Song(String name, String artist, int popularity) {
+        this.name = name;
+        this.artist = artist;
+        this.popularity = popularity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+}
