b7cf9222ef5f61cc4617dca7bc74ec34498cba79
yuxinduan20040830 <yuxinduan0830@gmail.com>
2023-10-30 15:55:37 -0400

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,24 @@
 package src.app;
 
+import src.interface_adapter.ViewManagerModel;
+import src.view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Group Project");
+        JFrame application = new JFrame("Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // The various View objects. Only one view is visible at a time.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // This keeps track of and manages which view is currently showing.
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
     }
 }
