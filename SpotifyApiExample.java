7892865c2f738bfa08cf8c4403623394d6c1ed32
mehtab0301 <rida.asif@mail.utoronto.ca>
2023-11-13 03:39:00 -0500

    September 13, 2023

--- SpotifyApiExample.java
+++ SpotifyApiExample.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class SpotifyApiExample {
     // Note that our token expires every hour, so it must be updated constantly for it to work. We use PostMan to get it done. 
-    private static final String token = "BQCyeRKvG8PVjdiGsfnV9ns66jnS3eHCl5FRhAwhIxQSb_JEgr12qscfjWaylFtB23a72Twobksn0r5jWdniYl0fJMhnKtY8e0EYKIIhIK9gEmmB8WdVcdjnI4MaPWNY3KGVfqfDQBKUGPFv1My5B50QdjgUH2LMwPA_wQ8b5aiOKN6xN5EKIBHshxVpgikA-zdaIk7s61_tbdFJZadJt-KloSYpPScvRXT7BOLL4zNhQCH5SfclOsb5Rm1yCQ_0Ty52rAt4NQ";
+    private static final String token = "BQCpE_Upju9D8m-EKULQ2OqCBWOgB1Vx47F3YmO6zvuzcWIauWroppykYnfFrMcVuQtMsJch831vCVrsEssVtxkezZ314G_aIE7JFJZhSVAAy1uhsgGEnJCa0c2_Q2ZbBoITzTnuYG4rW8lKTlLnl7MgsD1d1L3QfrWBlvCszqaiCF7UalQQT1pv1GpOVljGH8pLUs6IjgEWuwHTYnUjkhm8iiQaon4jANZqjg";
     private static final String url = "https://api.spotify.com/v1/browse/new-releases";
 
     public static void main(String[] args) {
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,37 @@
 package src.app;
 
+import src.interface_adapter.GenerateViewModel;
+import src.view.GenerateView;
+import src.view.ViewManager;
+import src.interface_adapter.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Group Project");
+        JFrame application = new JFrame("App");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // The various View objects. Only one view is visible at a time.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // This keeps track of and manages which view is currently showing.
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        GenerateViewModel generateViewModel = new GenerateViewModel();
+
+        GenerateView generateView = new GenerateView(generateViewModel);
+        views.add(generateView);
+
+        viewManagerModel.setActiveView(generateView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
     }
 }
