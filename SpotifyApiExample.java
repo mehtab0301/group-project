82e50c209add64c3661ba3b9f975803cebdc9dbd
mehtab0301 <rida.asif@mail.utoronto.ca>
2023-11-13 17:52:23 -0500

    Pushing the latest code

--- SpotifyApiExample.java
+++ SpotifyApiExample.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class SpotifyApiExample {
     // Note that our token expires every hour, so it must be updated constantly for it to work. We use PostMan to get it done. 
-    private static final String token = "BQBGiAGSWiQ6CFmyGLR9A-9sjh4dhgN-U4czUUGXUhqV_42c0ns3K0C3PXPEg4i9qOkUFI9nNweRy25y4hIlIfigHdKaQLM83BHPQnokiQQ4Yat8HJyuuB76_37UWsj2GQGgtK6V8JhfmArO-RkBlqApXra1UBIqo_ZbM2Sb7Aix9iVuNuqFelJXRjCLxnJLZIGdRNvGg5yU7euHVYq-vrsQoCUYyNyceYwiXw";
+    private static final String token = "BQCyeRKvG8PVjdiGsfnV9ns66jnS3eHCl5FRhAwhIxQSb_JEgr12qscfjWaylFtB23a72Twobksn0r5jWdniYl0fJMhnKtY8e0EYKIIhIK9gEmmB8WdVcdjnI4MaPWNY3KGVfqfDQBKUGPFv1My5B50QdjgUH2LMwPA_wQ8b5aiOKN6xN5EKIBHshxVpgikA-zdaIk7s61_tbdFJZadJt-KloSYpPScvRXT7BOLL4zNhQCH5SfclOsb5Rm1yCQ_0Ty52rAt4NQ";
     private static final String url = "https://api.spotify.com/v1/browse/new-releases";
 
     public static void main(String[] args) {
