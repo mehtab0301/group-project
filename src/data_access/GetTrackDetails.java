57d4bafa9dc2dd55a1eccd47c88b18ba25951267
yuxinduan20040830 <yuxinduan0830@gmail.com>
2023-11-17 20:58:17 -0500

    Adjusting file locations

--- src/data_access/GetTrackDetails.java
+++ src/data_access/GetTrackDetails.java
@@ -1,3 +1,5 @@
+package data_access;
+
 import okhttp3.*;
 import org.json.JSONObject;
 
@@ -11,7 +13,7 @@ public class GetTrackDetails {
         int index = songLink.indexOf("track/");
         String songID = songLink.substring(index + "track/".length()); //removes everything before the trackID
         String api_call = "https://api.spotify.com/v1/tracks/" + songID;
-        // the above just gets the track Id and combines it to the general api call
+        // the above just gets the track id and combines it to the general api call
         OkHttpClient client = new OkHttpClient(); //builds an http request
         Request request = new Request.Builder().url(api_call).addHeader("Authorization", "Bearer " + token ).build();
         Response songInfo = client.newCall(request).execute();
