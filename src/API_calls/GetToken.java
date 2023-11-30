ae4d5f0f873624b70ee66580287de0cbb204f864
NickPestov <nick.pestov@vgis.io>
2023-11-30 18:22:00 -0500

    Made default button values Genre: "Acoustic" and num_of_tracks = 1

--- src/API_calls/GetToken.java
+++ src/API_calls/GetToken.java
@@ -1,7 +1,7 @@
 package API_calls;
 
 public class GetToken {
-    private static final String token = "BQBUGoYA7UkiyyG0kEVPWv7axLpyFETuEIK_Xnf9dgVod5hmeFXJFn7ZLeLGTISWOZXgtpsRlvHNuAFracN0OS_dEOiuJd-VEVkDJCFmP91gWz3mTQnS7vEzp9c7doya8hZ2xJZCb25a-x5QdI6rFFuShBFt0XaIRfE3GQxu7kXtxlJprm-NRK8xxXr2bV5Sj3ZsI8JIA3xGY8z1tRj8gUBdZqrLoVJrO4diuf8-L29ZXOI5vjW39l_EoTPyDCRYiWjRf_tykg";
+    private static final String token = "BQDEQgcg9bvrmxmLVIVUseHH6Nd_ZPPdMrNYNR_V6U0loa1tuoQtLozGbnQmaG4TXzvUGG_tbqqgw_6puRoUBCxoekeLOX90SHRoEEAuWrMHGrViWB5jMmvmjGwNUMdXVEiYZm_vcPnZolz1niV1qtXLu8RXpw5tCDCcImgWLgVaEGI4zQE4ciw7R89LBXHT_KtxGcaf1tBeMEq-rKAR7x9FLkE1HPQbV-qHgJYlph9u3p-t4z8qKE-9uvpcfZHXtUUYVeBXCw";
 
     public static String getToken() {
         return token;
