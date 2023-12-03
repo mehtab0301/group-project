709ec8ba28676bdca4bdf3ba66f4a15b1700701d
NickPestov <nick.pestov@vgis.io>
2023-12-02 22:20:07 -0500

    Testing for Signup

--- test/use_case/signup/SignupInteractorTest.java
+++ test/use_case/signup/SignupInteractorTest.java
@@ -1,48 +1,75 @@
 package use_case.signup;
 
+import data_access.UserDataAccessObject;
+import entity.CommonUserFactory;
 import entity.User;
-import entity.UserFactory;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import use_case.login.*;
+import use_case.signup.*;
 
-public class SignupInteractorTest {
+import java.io.IOException;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class signUpInteractorTest {
     @Test
-    public void SignupTest() {
-        SignupInputData signupInputData = new SignupInputData("username", "pwd", "pwd");
+    public void testPrepareSuccessfulSignupView() throws IOException {
+        // Arrange
+        SignupOutputBoundary successPresenter = mock(SignupOutputBoundary.class);
+        SignupInputData signupInputData = new SignupInputData("newUser", "password123", "password123");
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject(new CommonUserFactory());
 
-        SignupOutputBoundary successPresenter = new SignupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(SignupOutputData user) {
-                Assert.assertNull(user);
-            }
+        // Create the SignupInteractor with the mock success presenter and a UserFactory
+        SignupInteractor signupInteractor = new SignupInteractor(userDataAccessObject, successPresenter, new CommonUserFactory());
 
-            @Override
-            public void prepareFailView(String error) {
-            }
-        };
-
-        SignupUserDataAccessInterface signupUserDataAccessInterface = new SignupUserDataAccessInterface() {
-            @Override
-            public boolean existsByName(String identifier) {
-                return false;
-            }
-
-            @Override
-            public void save(User user) {
-
-            }
-        };
-
-        UserFactory userFactory = new UserFactory() {
-            @Override
-            public User create(String name, String password) {
-                return null;
-            }
-        };
-
-        SignupInputBoundary signupInteractor = new SignupInteractor(signupUserDataAccessInterface, successPresenter,
-                userFactory);
+        // Act
         signupInteractor.execute(signupInputData);
+
+        // Assert
+        // Verify that prepareSuccessView was called
+        verify(successPresenter, times(1)).prepareSuccessView(any(SignupOutputData.class));
+    }
+    @Test
+    public void testPrepareFailPasswordSignupView() throws IOException {
+        // Arrange
+        SignupOutputBoundary failPresenter = mock(SignupOutputBoundary.class);
+        SignupInputData signupInputData = new SignupInputData("userTest", "123", "wrongPassword");
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject(new CommonUserFactory());
+        // Set up a user for the existing account
+        User user = new CommonUserFactory().create("userTest", "correctPassword");
+        userDataAccessObject.save(user);
+
+        // Create the SignupInteractor with the mock fail presenter
+        SignupInputBoundary signupInteractor = new SignupInteractor(userDataAccessObject, failPresenter,  new CommonUserFactory());
+
+        // Act
+        signupInteractor.execute(signupInputData);
+
+        // Assert
+        // Verify that prepareFailView was called with the expected error message
+        verify(failPresenter, times(1)).prepareFailView("Passwords don't match.");
+        // You might want to further assert specific details about the expected error message
+    }
+    @Test
+    public void testPrepareFailUserView() throws IOException {
+        // Arrange
+        SignupOutputBoundary failPresenter = mock(SignupOutputBoundary.class);
+        SignupInputData signupInputData = new SignupInputData("existingUser", "123", "password");
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject(new CommonUserFactory());
+
+        // Set up a user for the existing account
+        User existingUser = new CommonUserFactory().create("existingUser", "existingPassword");
+        userDataAccessObject.save(existingUser);
+
+        // Create the SignupInteractor with the mock fail presenter and a UserFactory
+        SignupInteractor signupInteractor = new SignupInteractor(userDataAccessObject, failPresenter, new CommonUserFactory());
+
+        // Act
+        signupInteractor.execute(signupInputData);
+
+        // Assert
+        // Verify that prepareFailView was called with the expected error message
+        verify(failPresenter, times(1)).prepareFailView("User already exists with the username 'existingUser'.");
     }
 }
