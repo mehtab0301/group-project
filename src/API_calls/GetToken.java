3ff3f47f23a968799d7a092075fda3ba0f0447a7
NickPestov <nick.pestov@vgis.io>
2023-12-03 22:32:46 -0500

    Testing for Signup

--- src/API_calls/GetToken.java
+++ src/API_calls/GetToken.java
@@ -1,7 +1,7 @@
 package API_calls;
 
 public class GetToken {
-    private static final String token = "BQA8UbbIAdbpUleaZhgUUJ3nc0NSTL6_JFcP_zaAjSFOh8TFOezJHfvJySgKm2emqTQ59VQc0HmF7wCIxNrQkqHCrCKVbJlI2Hw0Q4m7RP-wm2j4GzEgG_aw-nj5xmVhBKksmLMMHhogdm1g-C1KveFcSqrMuLrt4EeOgllixF2MA4ikKPkBTguuFSqA-6XaBY_Lg6M5nrnQM9taD999rQl-ekD1Xj9yR0BYjK9nV0q4PM0rnSCHWQ";
+    private static final String token = "BQDa_ZPmiZ_KAw2K1wGHKtLa3dupNTkafDzaAKP6GTgCH7mnmh4ofyS9uqtUN6oIfTEeKhsjjwdF_Yb_4ukVXl1uAKAREaPgIJDxGXvcCE4hqxobm478GniD3l0Ur7aJ-P5ez6IyJxhJEhPX-R1atu9iXdswCGPS0kF0bMbB03LeBofKQLqHkIj7ZisY-6Hs9VKUn2yzecK54HRJG-fzbeYDffV6aRrZ25VVFObyQ1fmpcwlUNBm2kLrb_yC4LKt108wW22bxA";
 
     public static String getToken() {
         return token;
